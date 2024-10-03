484ba025e508c661cbed6a0d17d1355b5800eff4
Purity Wambua <puritywamb@gmail.com>
2024-10-03 23:03:07 +0300

    remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # Send a GET request to the URL
+        response = requests.get(self.url)
+        # Raise an error for bad responses (4xx or 5xx)
+        response.raise_for_status()
+        # Return the body of the response
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+         #Parse the response body as JSON and return it
+        return json.loads(response_body)
